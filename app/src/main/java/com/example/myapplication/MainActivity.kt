fac6fc59f2dae35ade755fafe01d5bc3b2d58918
LAPTOP-IDGCA532\admin <narubet>
2022-02-14 11:26:13 +0700

    [Added] Splash screen xml

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.FragmentSplashScreenBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(FragmentSplashScreenBinding.inflate(layoutInflater).root)
     }
 }

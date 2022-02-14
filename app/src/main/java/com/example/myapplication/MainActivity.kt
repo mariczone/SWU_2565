eb822454108ba8339e3acf238082a9843ab00e69
LAPTOP-IDGCA532\admin <narubet>
2022-02-14 13:54:25 +0700

    [Added] Signin Layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.FragmentSplashScreenBinding
+import com.example.myapplication.databinding.FragmentSignInBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(FragmentSplashScreenBinding.inflate(layoutInflater).root)
+        setContentView(FragmentSignInBinding.inflate(layoutInflater).root)
     }
 }

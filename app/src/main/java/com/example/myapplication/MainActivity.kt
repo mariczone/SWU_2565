131426828830ee1c9e7e728c16d9480bf6a53d16
LAPTOP-IDGCA532\admin <narubet>
2022-02-15 15:38:21 +0700

    [Added] Splash screen for android 12++

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.myapplication.databinding.FragmentSignInBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(FragmentSignInBinding.inflate(layoutInflater).root)
     }
 }

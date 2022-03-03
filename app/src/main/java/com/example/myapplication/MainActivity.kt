d73eb72510c6d507c9ee125cccbdb6d7bceae252
LAPTOP-IDGCA532\admin <narubet>
2022-03-03 13:43:31 +0700

    [Added] Navigation for upload page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.FragmentUploadPhotoBinding
+import com.example.myapplication.databinding.TemplateUploadPhotoBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var activityMainBinding: ActivityMainBinding
+        private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
     }
 }
--- app/src/main/java/com/example/myapplication/UploadPhotoFragment.kt
+++ app/src/main/java/com/example/myapplication/UploadPhotoFragment.kt
@@ -5,22 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentUploadPhotoBinding
+import com.example.myapplication.databinding.TemplateUploadPhotoBinding
 
 class UploadPhotoFragment : Fragment() {
-    private lateinit var fragmentUploadPhotoBinding: FragmentUploadPhotoBinding
+    private lateinit var templateUploadPhotoBinding: TemplateUploadPhotoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        fragmentUploadPhotoBinding = FragmentUploadPhotoBinding.inflate(layoutInflater)
-        return fragmentUploadPhotoBinding.root
+        templateUploadPhotoBinding = TemplateUploadPhotoBinding.inflate(layoutInflater)
+
+        return templateUploadPhotoBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        templateUploadPhotoBinding.centerLayout.apply {
+            addView(FragmentUploadPhotoBinding.inflate(layoutInflater).root)
+        }
+        templateUploadPhotoBinding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }

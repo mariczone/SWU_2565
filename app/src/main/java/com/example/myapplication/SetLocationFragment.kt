36b7c8e43193da2df16f5662a85cf6276deab6ca
LAPTOP-IDGCA532\admin <narubet>
2022-03-18 01:10:14 +0700

    [Added] set location fragment

--- app/src/main/java/com/example/myapplication/SetLocationFragment.kt
+++ app/src/main/java/com/example/myapplication/SetLocationFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSetLocationBinding
+import com.example.myapplication.databinding.TemplateUploadPhotoBinding
 
 class SetLocationFragment : Fragment() {
 
     private lateinit var fragmentSetLocationBinding: FragmentSetLocationBinding
+    private lateinit var templateUploadPhotoBinding: TemplateUploadPhotoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +20,17 @@ class SetLocationFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         fragmentSetLocationBinding = FragmentSetLocationBinding.inflate(layoutInflater)
-        return fragmentSetLocationBinding.root
+        templateUploadPhotoBinding = TemplateUploadPhotoBinding.inflate(layoutInflater)
+        return templateUploadPhotoBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        templateUploadPhotoBinding.centerLayout.apply {
+            addView(fragmentSetLocationBinding.root)
+        }
+        templateUploadPhotoBinding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }

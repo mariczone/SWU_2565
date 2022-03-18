32df47d22cdc1bb249c8218ae50c9aa064a1592d
LAPTOP-IDGCA532\admin <narubet>
2022-03-18 08:02:25 +0700

    [Added] Request location

--- app/src/main/java/com/example/myapplication/SetLocationFragment.kt
+++ app/src/main/java/com/example/myapplication/SetLocationFragment.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSetLocationBinding
 import com.example.myapplication.databinding.TemplateUploadPhotoBinding
+import com.google.android.gms.location.*
+
+private val TAG = SetLocationFragment::class.java.simpleName
 
 class SetLocationFragment : Fragment() {
 
@@ -31,6 +36,32 @@ class SetLocationFragment : Fragment() {
         }
         templateUploadPhotoBinding.backBtn.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun setupLocationProvider() {
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+        val locationRequest =
+            LocationRequest().setInterval(6000L).setFastestInterval(1000L).setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY
+            )
+
+        val locationTask =
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        locationTask.addOnFailureListener {
+            Log.e(TAG, "Failure: ${it.message}")
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(currentLocation: LocationResult) {
+            super.onLocationResult(currentLocation)
+            Log.d(TAG, "onLocationResult: currentLocation $currentLocation")
         }
     }
 }

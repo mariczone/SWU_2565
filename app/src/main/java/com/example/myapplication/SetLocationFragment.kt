be8d985333ac39cdae35d1ce4bb7b9d441fa7359
LAPTOP-IDGCA532\admin <narubet>
2022-03-18 08:39:14 +0700

    [Added] request location permission

--- app/src/main/java/com/example/myapplication/SetLocationFragment.kt
+++ app/src/main/java/com/example/myapplication/SetLocationFragment.kt
@@ -1,16 +1,24 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSetLocationBinding
 import com.example.myapplication.databinding.TemplateUploadPhotoBinding
-import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 private val TAG = SetLocationFragment::class.java.simpleName
 
@@ -37,22 +45,46 @@ class SetLocationFragment : Fragment() {
         templateUploadPhotoBinding.backBtn.setOnClickListener {
             findNavController().popBackStack()
         }
+        fragmentSetLocationBinding.setLocationBtn.setOnClickListener {
+            setupLocationProvider()
+        }
     }
 
-    private fun setupLocationProvider() {
-        val fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireContext())
-        val locationRequest =
-            LocationRequest().setInterval(6000L).setFastestInterval(1000L).setPriority(
-                LocationRequest.PRIORITY_HIGH_ACCURACY
-            )
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                setupLocationProvider()
+                Toast.makeText(requireContext(), "Granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "Deny", Toast.LENGTH_LONG).show()
+            }
+        }
 
+    private fun setupLocationProvider() {
         val locationTask =
-            fusedLocationProviderClient.requestLocationUpdates(
-                locationRequest,
-                locationCallback,
-                Looper.getMainLooper()
-            )
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                return
+            } else {
+                val fusedLocationProviderClient =
+                    LocationServices.getFusedLocationProviderClient(requireContext())
+                val locationRequest =
+                    LocationRequest().setInterval(6000L).setFastestInterval(1000L).setPriority(
+                        LocationRequest.PRIORITY_HIGH_ACCURACY
+                    )
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+
         locationTask.addOnFailureListener {
             Log.e(TAG, "Failure: ${it.message}")
         }
